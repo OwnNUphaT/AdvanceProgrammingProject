a0f854c6d0f1c773f37b2dfa33b74f550a3f24a7
Philip Housden <philip.houden.04@gmail.com>
2023-09-09 12:51:27 +0700

    Adding the image into resource file and modifies the size of the application

--- src/main/java/com/advanceprogramproject/Launcher.java
+++ src/main/java/com/advanceprogramproject/Launcher.java
@@ -9,12 +9,12 @@ import java.io.IOException;
 
 public class Launcher extends Application {
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Launcher.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
+    public void start(Stage primarystage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Launcher.class.getResource("main-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 930, 480);
+        primarystage.setTitle("Watermark");
+        primarystage.setScene(scene);
+        primarystage.show();
     }
 
     public static void main(String[] args) {
