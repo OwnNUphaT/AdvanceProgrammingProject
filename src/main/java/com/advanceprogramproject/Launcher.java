3ee2643f8c5ed65d29db783f472abfe0384ceef0
Philip Housden <philip.houden.04@gmail.com>
2023-09-09 14:20:25 +0700

    Setting Pane and Button radius

--- src/main/java/com/advanceprogramproject/Launcher.java
+++ src/main/java/com/advanceprogramproject/Launcher.java
@@ -1,19 +1,25 @@
 package com.advanceprogramproject;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class Launcher extends Application {
+    @FXML
+    private Pane importPane;
     @Override
     public void start(Stage primarystage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Launcher.class.getResource("main-view.fxml"));
 
         Scene scene = new Scene(fxmlLoader.load(), 930, 480);
 
+        // Setting border radius
 
 
         primarystage.setResizable(false);
