a86e484e6132b40e5eae08f5e64909ca9bf48441
philip.houden.04@gmail.com <philip.houden.04@gmail.com>
2023-09-12 12:12:26 +0700

    Setting Drag-Drop event handler. So that user can Drag and Drop the image file into the Pane

--- src/main/java/com/advanceprogramproject/control/MainViewController.java
+++ src/main/java/com/advanceprogramproject/control/MainViewController.java
@@ -1,25 +1,57 @@
 package com.advanceprogramproject.control;
 
 import javafx.fxml.FXML;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.Dragboard;
+import javafx.scene.input.TransferMode;
 import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
 
 import java.io.File;
 
 public class MainViewController {
     @FXML
     private Pane importPane;
-
+    @FXML
+    private ImageView importImage;
+    @FXML
+    private Label importLabel;
+    private Stage stage;
+    private Scene scene;
     public void initialize() {
+        // Setting OnDragOver
+        importPane.setOnDragOver(event -> {
+            if (event.getGestureSource() != importPane && event.getDragboard().hasFiles()) {
+                event.acceptTransferModes(TransferMode.COPY);
+            }
+            event.consume();
+        });
 
-        // TODO: Setting OnDragOver and OnDragDropped when the file have been use
+        // Setting OnDragDropped
         importPane.setOnDragDropped(event -> {
-
+            Dragboard db = event.getDragboard();
+            boolean success = false;
+            if (db.hasFiles()) {
+                // Handle the dropped files here
+                for (File file : db.getFiles()) {
+                    // You can process the file here
+                    System.out.println("Dropped file: " + file.getAbsolutePath());
+                }
+                success = true;
+                //after the user have input the file into the list view the import icons will be disappeared
+                importImage.setVisible(false);
+                importLabel.setVisible(false);
+            }
+            event.setDropCompleted(success);
+            event.consume();
         });
 
         // TODO: Let the user able to choose within the browser to choose the file
 
-        //TODO: after the user have input the file into the list view the import icons will be disappear
+
+
     }
 }
