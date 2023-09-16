4ec16f2f0e8e0ed62567481fa5549919c45f13c5
philip.houden.04@gmail.com <philip.houden.04@gmail.com>
2023-09-16 22:50:01 +0700

    Adding @FXML tags and TODOs

--- src/main/java/com/advanceprogramproject/control/imagePageController.java
+++ src/main/java/com/advanceprogramproject/control/imagePageController.java
@@ -1,7 +1,10 @@
 package com.advanceprogramproject.control;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
 import javafx.stage.Stage;
 
 import java.net.URL;
@@ -9,21 +12,24 @@ import java.util.ResourceBundle;
 
 public class imagePageController implements Initializable {
     private Stage stage;
-    private Scene scene;
+    @FXML
+    private Label dimensionLabel;
+    @FXML
+    private Label qualityLabel;
+    @FXML
+    private Slider dimensionSlider;
+    @FXML
+    private Slider qualitySlider;
 
     public void setStage(Stage stage) {
         this.stage = stage;
     }
 
-    // Method to close the previous scene
-    public void closePreviousScene() {
-        if (stage != null) {
-            stage.close();
-        }
-    }
+
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        //TODO: Make all slider Label display the value of the slider under the slider
 
     }
 }
