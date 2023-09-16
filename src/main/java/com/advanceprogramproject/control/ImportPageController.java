856fcc4acbab0c24dbfbee30f65404eff2e25043
philip.houden.04@gmail.com <philip.houden.04@gmail.com>
2023-09-16 22:19:26 +0700

    Adding scene

--- src/main/java/com/advanceprogramproject/control/ImportPageController.java
+++ src/main/java/com/advanceprogramproject/control/ImportPageController.java
@@ -1,15 +1,20 @@
 package com.advanceprogramproject.control;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class ImportPageController implements Initializable {
     private Stage stage;
+    private Scene scene;
 
     @FXML
     private Button imageBtn;
@@ -31,7 +36,25 @@ public class ImportPageController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         imageBtn.setOnAction(event -> {
-            stage.close();
+            try{
+                //Closing the current scene
+                closePreviousScene();
+
+                //Loading new fxml file
+                FXMLLoader loader = new FXMLLoader(ImportPageController.class.getResource("/com/advanceprogramproject/views/image-page.fxml"));
+                Parent root = loader.load();
+
+                // Pass the current stage reference to the new controller
+                imagePageController controller = loader.getController();
+                controller.setStage(stage);
+
+                scene = new Scene(root);
+                stage.setScene(scene);
+                stage.show();
+
+            }catch (IOException e) {
+                e.printStackTrace();
+            }
         });
 
         textBtn.setOnAction(event -> {
--- src/main/java/com/advanceprogramproject/control/imagePageController.java
+++ src/main/java/com/advanceprogramproject/control/imagePageController.java
@@ -1,13 +1,29 @@
 package com.advanceprogramproject.control;
 
+import javafx.fxml.Initializable;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class imagePageController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class imagePageController implements Initializable {
     private Stage stage;
     private Scene scene;
 
-    public void Initialize() {
+    public void setStage(Stage stage) {
+        this.stage = stage;
+    }
+
+    // Method to close the previous scene
+    public void closePreviousScene() {
+        if (stage != null) {
+            stage.close();
+        }
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
 
     }
 }
