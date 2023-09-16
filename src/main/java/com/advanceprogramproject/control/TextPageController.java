16f9a5be3eeddca59445aac6835c7b0c6b9c24f3
philip.houden.04@gmail.com <philip.houden.04@gmail.com>
2023-09-16 22:46:54 +0700

    Adding @FXML tags and TODOs

--- src/main/java/com/advanceprogramproject/control/TextPageController.java
+++ src/main/java/com/advanceprogramproject/control/TextPageController.java
@@ -1,7 +1,10 @@
 package com.advanceprogramproject.control;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
 import javafx.stage.Stage;
 
 import java.net.URL;
@@ -9,7 +12,20 @@ import java.util.ResourceBundle;
 
 public class TextPageController implements Initializable {
     private Stage stage;
-    private Scene scene;
+    @FXML
+    private Label visibilityLabel;
+    @FXML
+    private Label paddingLabel;
+    @FXML
+    private Label sizeLabel;
+    @FXML
+    private Slider VisibilitySlider;
+    @FXML
+    private Slider paddSlider;
+    @FXML
+    private Slider sizeSlider;
+
+
 
     public void setStage(Stage stage) {
         this.stage = stage;
@@ -23,6 +39,8 @@ public class TextPageController implements Initializable {
     }
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+
+        //TODO: Make all slider Label display the value of the slider under the slider
 
     }
 }
