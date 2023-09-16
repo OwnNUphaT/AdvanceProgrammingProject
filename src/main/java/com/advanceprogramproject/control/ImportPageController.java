586643d9d1966b613751cbdf02539453a625bc0b
philip.houden.04@gmail.com <philip.houden.04@gmail.com>
2023-09-16 19:53:59 +0700

    Adding new controller for the imported-page.fxml, ImportPageController.java

---
+++ src/main/java/com/advanceprogramproject/control/ImportPageController.java
@@ -0,0 +1,34 @@
+package com.advanceprogramproject.control;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+public class ImportPageController {
+    private Stage stage;
+
+    @FXML
+    private Button imageBtn;
+
+    @FXML
+    private Button textBtn;
+
+    public void setStage(Stage stage) {
+        this.stage = stage;
+    }
+
+    // Method to close the previous scene
+    public void closePreviousScene() {
+        if (stage != null) {
+            stage.close();
+        }
+    }
+
+    public void Initialize() {
+
+        imageBtn.setOnAction(event -> {
+            System.out.println("I have been click!");
+        });
+
+    }
+}
